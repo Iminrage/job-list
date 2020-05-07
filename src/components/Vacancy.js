db3db0435ea94d3e8fd14b01054103e55350ab9d
folkaras <efolkaras@gmail.com>
2020-05-08 01:38:44 +0300

    add vacancy comp 0.1

---
+++ src/components/Vacancy.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./vacancy.scss";
+export default (props) => {
+  return (
+    <div className="vacancy">
+      <div className="vacancy__logo-wrapper">
+        <img
+          src={require(`../img/${props.logo}.svg`)}
+          alt={props.logo}
+          className="vacancy__logo-wrapper img-responsive"
+        />
+      </div>
+      <div className="vacancy__description">
+        <div className="vacancy__row">
+          <div className="vacancy__employer">{props.employer}</div>
+          <div className="vacancy__labels"></div>
+        </div>
+        <div className="vacancy__title-wrapper vacancy__row">
+          <h2 className="vacancy__title">{props.title}</h2>
+        </div>
+        <div className="vacancy__special-info vacancy__row">
+          <div className="special-info__item vacancy__date">{props.date}</div>
+          <div className="special-info__item vacancy__employment-type">
+            {props.empType}
+          </div>
+          <div className="special-info__item vacancy__residence">
+            {props.residence}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};

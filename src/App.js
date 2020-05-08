88b6d37cfc28f568e995be23105777799da462ce
folkaras <efolkaras@gmail.com>
2020-05-09 01:47:45 +0300

    bind tag click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Vacancy from "./components/Vacancy";
 import "./app.scss";
 
 function App() {
-  const vacancies = require("./data.json");
+  let vacancies = require("./data.json");
+  const [activeTags, setActiveTag] = useState([]);
+  const getTag = (tag) => {
+    setActiveTag([...activeTags, tag]);
+    console.log(activeTags);
+  };
   return (
     <div className="app">
       <div className="container">
+        {activeTags}
         <div className="vacancies">
           {vacancies.map((vacancy) => {
             return (
@@ -25,7 +31,8 @@ function App() {
                   vacancy.level,
                   vacancy.languages,
                   vacancy.tools,
-                ]}></Vacancy>
+                ]}
+                click={getTag}></Vacancy>
             );
           })}
         </div>

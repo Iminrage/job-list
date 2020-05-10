0ba2fbbc0c1aa684e31025d9670f4258fb104f35
folkaras <efolkaras@gmail.com>
2020-05-10 20:33:15 +0300

    Updates

---
+++ precache-manifest.ea5c2fd47cf00a1542065465458399f3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17b70b9ab52d93d40cfd02b1a7d6955",
+    "url": "/joblist/index.html"
+  },
+  {
+    "revision": "55991700b393ffb554b3",
+    "url": "/joblist/static/css/main.533ce320.chunk.css"
+  },
+  {
+    "revision": "7441db47da8b498c9595",
+    "url": "/joblist/static/js/2.07534e64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joblist/static/js/2.07534e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55991700b393ffb554b3",
+    "url": "/joblist/static/js/main.765b00fa.chunk.js"
+  },
+  {
+    "revision": "613907fd3a292b3afb1c",
+    "url": "/joblist/static/js/runtime-main.2ea3588d.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/joblist/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/joblist/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/joblist/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/joblist/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/joblist/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/joblist/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/joblist/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/joblist/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/joblist/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/joblist/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/joblist/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/joblist/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/joblist/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);

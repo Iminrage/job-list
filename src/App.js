b16c85543e634f83b16c5fac4a1e730790c6d55d
folkaras <efolkaras@gmail.com>
2020-05-10 20:17:22 +0300

    add filtration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Vacancy from "./components/Vacancy";
+import Filter from "./components/Filter";
 import "./app.scss";
 
 function App() {
   let vacancies = require("./data.json");
+  const [filteredVacancies, setFilteredVacancies] = useState(vacancies);
   const [activeTags, setActiveTag] = useState([]);
-  const getTag = (tag) => {
+  const [height, setHeight] = useState("");
+  function getTag(tag) {
     setActiveTag([...activeTags, tag]);
-    console.log(activeTags);
-  };
+    setHeight("increase");
+  }
+  function removeTag(tag) {
+    console.log("asdf");
+    let newArr = activeTags;
+    newArr.splice(newArr.indexOf(tag), 1);
+    setActiveTag([...newArr]);
+    setHeight("decrease");
+  }
+  function clear() {
+    setActiveTag([]);
+    setHeight("decrease");
+  }
+  function filterVacancies(change) {
+    function filterV(vacancy) {
+      let allTags = [vacancy.role, vacancy.level];
+      if (vacancy.tools) allTags = [...allTags, ...vacancy.tools];
+      if (vacancy.languages) allTags = [...allTags, ...vacancy.languages];
+      return activeTags.every((tag) => {
+        return allTags.includes(tag);
+      });
+    }
+    if (change === "increase") {
+      setFilteredVacancies(filteredVacancies.filter(filterV));
+    } else if (change === "decrease") {
+      setFilteredVacancies(vacancies.filter(filterV));
+    }
+  }
+  useEffect(() => {
+    filterVacancies(height);
+  }, [activeTags]);
   return (
     <div className="app">
       <div className="container">
-        {activeTags}
+        {activeTags.length > 0 && (
+          <div className="filter-wrapper">
+            <Filter
+              tags={activeTags}
+              removeTag={removeTag}
+              clear={clear}></Filter>
+          </div>
+        )}
         <div className="vacancies">
-          {vacancies.map((vacancy) => {
+          {filteredVacancies.map((vacancy) => {
             return (
               <Vacancy
                 key={vacancy.id}
